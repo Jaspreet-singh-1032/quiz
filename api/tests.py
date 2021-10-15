aa1340a98cb201ae68d3ed079670a7c452851cec
Jaspreet-singh-1032 <jassimehra2612@gmail.com>
2021-10-15 19:09:14 +0530

    testcases for questions api

--- api/tests.py
+++ api/tests.py
@@ -1,9 +1,25 @@
+import json
+
 from django.test import TestCase
+from django.test.client import Client
 from django.urls import reverse
 
 from rest_framework import status
 
+# models
 from django.contrib.auth import get_user_model
+from app.models import (
+    Quiz
+)
+
+def authenticate_client(client : Client) -> None:
+        '''
+        call this method to make client authenticated
+        '''
+        user = get_user_model().objects.create(username='test')
+        user.set_password('test')
+        user.save()
+        client.login(username='test' , password='test')
 
 class QuizApiTestCase(TestCase):
 
@@ -24,10 +40,7 @@ class QuizApiTestCase(TestCase):
         '''
         create quiz for authenticated user
         '''
-        user = get_user_model().objects.create(username='test')
-        user.set_password('test')
-        user.save()
-        self.client.login(username='test' , password='test')
+        authenticate_client(self.client)
         data = {
             "name":"test quiz"
         }
@@ -35,14 +48,105 @@ class QuizApiTestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data.get('name'), data.get('name'))
     
-    def test_create_quiz_authenticated_with_empty_data(self):
+    def test_create_quiz_authenticated_with_empty_data_fails(self):
         '''
         create quiz for authenticated user
         '''
-        user = get_user_model().objects.create(username='test')
-        user.set_password('test')
-        user.save()
-        self.client.login(username='test' , password='test')
+        authenticate_client(self.client)
         data = {}
         response = self.client.post(self.url , data = data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+class QuestionApiTestCase(TestCase):
+
+
+    def setUp(self):
+        # setup url with invalid quiz id
+        self.url = reverse('question-list' , kwargs={'quiz_pk' :1})        
+    
+    def test_add_question_unautnenticated_fails(self):
+        response = self.client.post(self.url , data={})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
+    def test_add_question_authenticated_empty_data_fails(self):
+        '''
+        return 400 if data is empty
+        '''
+        authenticate_client(self.client)
+        response = self.client.post(self.url,  data={} , content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def test_add_question_autheticated_empty_options_fails(self):
+        '''
+        return 400 is options is empty
+        '''
+        authenticate_client(self.client)
+        data = {
+            'text':"test question",
+            "options":[]
+        }
+        response = self.client.post(self.url , data=data , content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_add_question_authenticated_only_one_option_fails(self):
+        '''
+        atleast 2 options is needed
+        '''
+        authenticate_client(self.client)
+        data = {
+            "text":"test question",
+            "options":[
+                {
+                    "text":"test option",
+                    "is_correct":True
+                }
+            ]
+        }
+        response = self.client.post(self.url , data = data , content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def test_add_question_invalid_quiz_id_fails(self):
+        authenticate_client(self.client)
+        data = {
+            "text":"test question",
+            "options":[
+                {
+                    "text":"test option",
+                    "is_correct":True
+                },
+                {
+                    "text":"test option 2"
+                }
+            ]
+        }
+        response = self.client.post(self.url , data=data , content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_add_question(self):
+        authenticate_client(self.client)
+        data = {
+            "text":"test question",
+            "options":[
+                {
+                    "text":"test option",
+                    "is_correct":True
+                },
+                {
+                    "text":"test option 2"
+                }
+            ]
+        }
+        user = get_user_model().objects.all().get(username = 'test') # created this user in authenticated_client method
+        quiz = Quiz.objects.create(name='test quiz' , user = user)
+        url = reverse('question-list' , kwargs={'quiz_pk' :quiz.pk})    
+        response = self.client.post(url , data = data , content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_list_questions_unauthentcated_fails(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_list_questions_authentcated(self):
+        authenticate_client(self.client)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
